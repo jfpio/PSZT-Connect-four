bc5a447b3a7f5e4d5feacd16a41834b90f3a0924
Jan Piotrowski <j.piotrowski@millionmonkeys.pl>
2020-04-16 09:46:14 +0200

    Add depth argument to ai move

--- backend/app_test.py
+++ backend/app_test.py
@@ -13,7 +13,8 @@ def client():
 
 def test_game_initialize(client):
     input_data = {
-        'board': [[0 for x in range(7)] for y in range(6)]
+        'board': [[0 for x in range(7)] for y in range(6)],
+        'depth': 10,
     }
     excepted_data = [[0 for x in range(7)] for y in range(6)]
 
--- backend/game.py
+++ backend/game.py
@@ -3,9 +3,9 @@ from flask import request, jsonify
 
 def get_ai_move():
     data = request.get_json()
-    board = calculate_move(data['board'])
+    board = calculate_move(data['board'], data['depth'])
     return jsonify(board)
 
 
-def calculate_move(board):
+def calculate_move(board, depth):
     return board
